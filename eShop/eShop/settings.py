4c596a7079f8c6ddcb10fc7bdd02af095da4cf3f
ale22gor <dmitryegorenko@gmail.com>
2018-12-14 18:59:21 +0300

    +docker-compouse

--- eShop/eShop/settings.py
+++ eShop/eShop/settings.py
@@ -141,6 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = '/home/dmitry/WorkSpace/eShop/serverStatic'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
